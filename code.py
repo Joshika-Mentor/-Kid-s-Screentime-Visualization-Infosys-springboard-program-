02b09639ee947c527bb30877d11b1cdbb660a2fb
Vicky02062000 <vickyramchandar332@gmail.com>
2025-10-09 09:23:45 +0000

    Added my first update

--- code.py
+++ code.py
@@ -1,1 +1,10 @@
+# Here first we are importing Pandas library and connecting the drive for the data set
 import pandas as pd
+df="screen_time.csv"
+df=pd.read_csv("/content/drive/MyDrive/Dateset/screen_time.csv")
+df.head(10) #getting the first 10 rows of data
+df.tail(10) #getting the Last 10 rows of data
+df.describe() # getting the general describetion of the table
+
+df1=df[["Primary_Device", "Age"]]
+df1 #checking two columns in the table 
