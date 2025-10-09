e2070fb04a7b80593f9f1364c35e29b88456ea9c
Vicky02062000 <vickyramchandar332@gmail.com>
2025-10-09 02:46:43 -0700

    cleaning the data set

--- code.py
+++ code.py
@@ -8,3 +8,28 @@ df.describe() # getting the general describetion of the table
 
 df1=df[["Primary_Device", "Age"]]
 df1 #checking two columns in the table 
+
+df1=df[["Primary_Device", "Age"]][1:10:1]
+df1 #slicing the column data
+
+#to find number of rows and columns will use shape
+df.shape
+
+#check for missing values will use isnull and number of null values will use isnull().sum()
+df.isnull().sum()
+
+#check the duplicate
+df.duplicated().sum()
+
+#drop duplicates
+df.drop_duplicates(inplace=True)
+
+df.info() #info after dropping the duplicates
+
+#replacing the null value with 0
+df['Health_Impacts'] = df['Health_Impacts'].fillna(df['Health_Impacts'].mode()[0])
+df.isnull().sum()
+
+df['Gender']=df['Gender'].astype('category')
+df['Urban_or_Rural'].astype('category')
+print(df)# setting the as type
